d052d3f319b4a32d0e49a3100677ea7fd0ce7f6a
Ranjith Zachariah <ranjith.zachariah@gmail.com>
2015-11-30 14:55:18 -0500

    Add health endpoint

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/hello')
 def hello_world():
     return 'Hello from Reading, PA!'
+
+@app.route('/health')
+def health_check():
+    return jsonify({'status':'up'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
